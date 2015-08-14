a93f4dadb543fc546a35b8ff3b2a698cfd84acb9
Hugbot <loganknecht@gmail.com>
2015-08-13 23:05:38 -0700

    I did more of HW01, fixed output to match expected function output. Two exercises left.

--- CIS194IntroToHaskell/Week1/Homework/HW01.hs
+++ CIS194IntroToHaskell/Week1/Homework/HW01.hs
@@ -1,5 +1,8 @@
 {-# OPTIONS_GHC -Wall #-}
 module HW01 where
+-- I found a really helpful github here, particularly the most helpful portion was
+-- seeing how to access iteratively the first index of a digit
+-- https://github.com/TomPeters/haskell-homework/blob/master/Week01.hs
 
 -- Exercise 1 -----------------------------------------
 
@@ -7,37 +10,47 @@ module HW01 where
 lastDigit :: Integer -> Integer
 lastDigit number = read ((last (show number)):[])
 
----- Drop the last digit from a number
---dropLastDigit :: Integer -> Integer
---dropLastDigit = undefined
+-- Drop the last digit from a number
+dropLastDigit :: Integer -> Integer
+dropLastDigit number = read (init (show number))
 
----- Exercise 2 -----------------------------------------
+-- Exercise 2 -----------------------------------------
+toRevDigits :: Integer -> [Integer]
+toRevDigits number = reverse(toDigitsList number)
 
---toRevDigits :: Integer -> [Integer]
---toRevDigits = undefined
+toDigitsList :: Integer -> [Integer]
+toDigitsList number
+    | number < 1 = []
+    | otherwise = toDigitsList (number `div` 10) ++ [ number `mod` 10]
 
----- Exercise 3 -----------------------------------------
+-- Exercise 3 -----------------------------------------
 
----- Double every second number in a list starting on the left.
---doubleEveryOther :: [Integer] -> [Integer]
---doubleEveryOther = undefined
+-- Double every second number in a list starting on the left.
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther [a] = [a]
+doubleEveryOther (x:y:xs) = x : y * 2 : (doubleEveryOther xs)
 
----- Exercise 4 -----------------------------------------
+-- Exercise 4 -----------------------------------------
 
----- Calculate the sum of all the digits in every Integer.
---sumDigits :: [Integer] -> Integer
---sumDigits = undefined
+-- Calculate the sum of all the digits in every Integer.
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits [a] = sumDigit a 
+sumDigits (x:xs) = (sumDigit x) + (sumDigits xs)
 
+sumDigit :: Integer -> Integer
+sumDigit number = if number < 10 then number else (sumDigit (number `div` 10)) + (number `mod` 10)
 
----- Exercise 5 -----------------------------------------
+-- Exercise 5 -----------------------------------------
 
----- Validate a credit card number using the above functions.
+-- Validate a credit card number using the above functions.
 --luhn :: Integer -> Bool
 --luhn = undefined
 
----- Exercise 6 -----------------------------------------
+-- Exercise 6 -----------------------------------------
 
----- Towers of Hanoi for three pegs
+-- Towers of Hanoi for three pegs
 --type Peg = String
 --type Move = (Peg, Peg)
 
